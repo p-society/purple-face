101c97eab80d4540f8ddcd98a288df40dc9836de
Spandan Hota <spandanhota2005@outlook.com>
2025-07-11 13:38:46 +0530

    feat: create login page
    Signed-off-by: Spandan Hota <spandanhota2005@outlook.com>

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -1,12 +1,11 @@
-import Header from "@/components/LandingPage/header";
 import Footer from "@/components/LandingPage/footer";
+import Header from "@/components/LandingPage/header";
 import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
-
-    const redirectToGoogle = () => {
-    window.location.assign("/auth/google");
-    };
+	const redirectToGoogle = () => {
+		window.location.assign("/auth/google");
+	};
 
 	return (
 		<div className="flex flex-col min-h-screen">
@@ -89,31 +88,33 @@ export default function LoginPage() {
 
 							<Button
 								size="lg"
-								onClick= { redirectToGoogle }
+								onClick={redirectToGoogle}
 								className="neo-brutal-button bg-foreground text-black hover:bg-primary/90 border-primary text-lg px-8 py-6 ghibli-button"
 							>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 533.5 544.3"
-                                className="w-5 h-5"
-                            >
-                                <path
-                                fill="#4285f4"
-                                d="M533.5 278.4c0-17.8-1.5-35-4.4-51.6H272v97.7h146.9c-6.4 34.5-25 63.7-53.5 83.2v68h86.5c50.7-46.7 81.6-115.4 81.6-197.3z"
-                                />
-                                <path
-                                fill="#34a853"
-                                d="M272 544.3c72.4 0 133-24 177.3-65.5l-86.5-68c-24 16.1-54.6 25.7-90.8 25.7-69.8 0-128.9-47.1-150.2-110.4h-89.4v69.4C85 477.1 170.5 544.3 272 544.3z"
-                                />
-                                <path
-                                fill="#fbbc04"
-                                d="M121.8 326.1c-10-30-10-62.4 0-92.4v-69.4H32.4c-38.7 77.5-38.7 170.7 0 248.2l89.4-69.4z"
-                                />
-                                <path
-                                fill="#ea4335"
-                                d="M272 107.7c39.4 0 74.8 13.6 102.5 40.3l76.8-76.8C405 24 344.4 0 272 0 170.5 0 85 67.2 32.4 164.3l89.4 69.4C143.1 154.8 202.2 107.7 272 107.7z"
-                                />
-                            </svg>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 533.5 544.3"
+									className="w-5 h-5"
+                                    role="img"
+                                    aria-label="google-icon-title"
+								>
+									<path
+										fill="#4285f4"
+										d="M533.5 278.4c0-17.8-1.5-35-4.4-51.6H272v97.7h146.9c-6.4 34.5-25 63.7-53.5 83.2v68h86.5c50.7-46.7 81.6-115.4 81.6-197.3z"
+									/>
+									<path
+										fill="#34a853"
+										d="M272 544.3c72.4 0 133-24 177.3-65.5l-86.5-68c-24 16.1-54.6 25.7-90.8 25.7-69.8 0-128.9-47.1-150.2-110.4h-89.4v69.4C85 477.1 170.5 544.3 272 544.3z"
+									/>
+									<path
+										fill="#fbbc04"
+										d="M121.8 326.1c-10-30-10-62.4 0-92.4v-69.4H32.4c-38.7 77.5-38.7 170.7 0 248.2l89.4-69.4z"
+									/>
+									<path
+										fill="#ea4335"
+										d="M272 107.7c39.4 0 74.8 13.6 102.5 40.3l76.8-76.8C405 24 344.4 0 272 0 170.5 0 85 67.2 32.4 164.3l89.4 69.4C143.1 154.8 202.2 107.7 272 107.7z"
+									/>
+								</svg>
 								Login with Google
 							</Button>
 						</div>
